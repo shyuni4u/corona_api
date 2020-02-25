0ff5504adcb77425948169fef6c089287e528ab5
shyuni4u <shyuniz@naver.com>
2020-02-25 19:18:13 +0900

    gitinit

---
+++ local.js
@@ -0,0 +1,23 @@
+const mysql     = require('mysql');
+const config    = require('./config.json');
+const mysqlPool = mysql.createPool ({ 
+    host    : config.npd.host, 
+    user    : config.npd.user,
+    password: config.npd.password,
+    database: config.npd.database,
+    connectionLimit : 60
+});
+const createResponse = (status, body) => ({
+    statusCode: status,
+    body: JSON.stringify(body)
+  });
+mysqlPool.getConnection(function(err, connection){
+    if(err !== null)
+        return console.log(createResponse(500, {message: err}));
+    connection.query('SELECT 1 AS RESULT', function(error,results,field) {
+        connection.release();
+        if(error !== null) 
+            return console.log(createResponse(500, {message: error}));
+        console.log(createResponse(200, {message: results[0].RESULT}));
+    });
+});
